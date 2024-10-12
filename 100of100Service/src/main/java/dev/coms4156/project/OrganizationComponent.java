4669a2f5b2717a4250dc6dd7134f945ba441712f
Alex Jiakai Xu <ax2155@columbia.edu>
2024-10-11 20:40:18 -0400

    Create OrganizationComponent interface

---
+++ 100of100Service/src/main/java/dev/coms4156/project/OrganizationComponent.java
@@ -0,0 +1,8 @@
+package dev.coms4156.project;
+
+/**
+ * This interface is used to represent an organization component.
+ * Designed under the Composite Design Pattern.
+ */
+public interface OrganizationComponent {
+}
