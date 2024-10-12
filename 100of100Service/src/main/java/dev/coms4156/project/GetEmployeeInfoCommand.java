3a84f2e56d4e4e04f636a2b91ab63e47bff8188a
Alex Jiakai Xu <ax2155@columbia.edu>
2024-10-12 02:13:05 -0400

    Implement Command interface and demo command

---
+++ 100of100Service/src/main/java/dev/coms4156/project/GetEmployeeInfoCommand.java
@@ -0,0 +1,23 @@
+package dev.coms4156.project;
+
+public class GetEmployeeInfoCommand implements Command {
+  private final int clientId;
+  private final int employeeId;
+
+  /**
+   * Constructs a command to get employee information.
+   * @param clientId the client ID
+   * @param employeeId the employee ID
+   */
+  public GetEmployeeInfoCommand(int clientId, int employeeId) {
+    this.clientId = clientId;
+    this.employeeId = employeeId;
+  }
+
+  @Override
+  public Object execute() {
+    HRDatabaseFacade db = HRDatabaseFacade.getInstance(this.clientId);
+    Employee employee = db.getEmployee(this.employeeId);
+    return employee.toString();
+  }
+}
