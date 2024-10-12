33089e9a55fc229365aabd1d81bc0e179abc5658
Alex Jiakai Xu <ax2155@columbia.edu>
2024-10-11 20:22:44 -0400

    Add basic SpringBoot routes

--- 100of100Service/src/main/java/dev/coms4156/project/RouteController.java
+++ 100of100Service/src/main/java/dev/coms4156/project/RouteController.java
@@ -1,8 +1,32 @@
 package dev.coms4156.project;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * This class contains all the API routes for the system.
  */
+@RestController
 public class RouteController {
+
+  /**
+   * Redirects to the homepage.
+   */
+  @GetMapping({"/", "/index", "/home"})
+  public String index() {
+    return "Welcome, in order to make an API call direct your browser or Postman to an endpoint.";
+  }
+
+
+  /**
+   * Handles any exceptions that occur in controller.
+   */
+  private ResponseEntity<?> handleException(Exception e) {
+    System.out.println(e.toString());
+    return new ResponseEntity<>("An Error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
 
 }
